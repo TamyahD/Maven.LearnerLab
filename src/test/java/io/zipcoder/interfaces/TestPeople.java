27b57c5d43526d33ee19f58cf7bf5c673acc3b49
Myah <myah@Zipcoders-MacBook-Pro-61.local>
2022-07-22 17:21:32 -0400

    completed part6.0

--- src/test/java/io/zipcoder/interfaces/TestPeople.java
+++ src/test/java/io/zipcoder/interfaces/TestPeople.java
@@ -5,21 +5,39 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TestPeople {
-    Person
+//    Person newPerson = new Person("Mario", 33333);
+    People<Person> personPeople = new People<>();
 
     @Test
-    public void add() {
+    public void testAdd() {
+        personPeople.add(new Person("Mario", 33333));
+
+        assertFalse(personPeople.personList.isEmpty());
+        assertSame("Mario",personPeople.personList.get(0).getName());
     }
 
     @Test
-    public void findById() {
-    }
+    public void testFindById() {
+        personPeople.add(new Person("Luigi", 77777));
+        long expectedId = 77777;
+        String expectedName = "Luigi";
 
-    @Test
-    public void remove() {
+        assertNotNull(personPeople.findById(expectedId));
+        assertSame(expectedName,personPeople.personList.get(0).getName());
+        assertEquals(expectedId, personPeople.personList.get(0).getId());
     }
 
     @Test
     public void testRemove() {
+        personPeople.add(new Person("Toad", 98765));
+        personPeople.add(new Person("Wario", 12345));
+        byte expectedListSize = 1;
+        String expectedName = "Toad";
+        long expectedId = 12345;
+
+        personPeople.remove(12345);
+        assertEquals(expectedListSize, personPeople.personList.size()); //equal list size
+        assertSame(expectedName, personPeople.personList.get(0).getName()); //correct person name return
     }
+
 }
